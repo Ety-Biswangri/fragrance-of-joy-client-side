fd17575c9084f489fd07eed50f6e0377b1613b02
Ety-Biswangri <etybgc02@gmail.com>
2022-05-07 17:23:26 +0600

    slider caption modified

--- src/components/HomePage/BannerSection/BannerSection.js
+++ src/components/HomePage/BannerSection/BannerSection.js
@@ -3,42 +3,43 @@ import { Carousel } from 'react-bootstrap';
 import banner1 from '../../../images/perfume-banner1.jpg';
 import banner2 from '../../../images/perfume-banner2.jpg';
 import banner3 from '../../../images/perfume-banner3.jpg';
+import './BannerSection.css';
 
 const BannerSection = () => {
     return (
         <div>
             <Carousel fade>
-                <Carousel.Item>
+                <Carousel.Item className='image-background'>
                     <img style={{ height: "35rem" }}
-                        className="d-block w-100 fluid"
+                        className="d-block w-100 fluid slide-image"
                         src={banner1}
                         alt="First slide"
                     />
-                    <Carousel.Caption>
+                    <Carousel.Caption className='slide-caption'>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item className='image-background'>
                     <img style={{ height: "35rem" }}
-                        className="d-block w-100 fluid"
+                        className="d-block w-100 fluid slide-image"
                         src={banner2}
                         alt="Second slide"
                     />
 
-                    <Carousel.Caption>
+                    <Carousel.Caption className='slide-caption'>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item className='image-background'>
                     <img style={{ height: "35rem" }}
-                        className="d-block w-100 fluid"
+                        className="d-block w-100 fluid slide-image"
                         src={banner3}
                         alt="Third slide"
                     />
 
-                    <Carousel.Caption>
+                    <Carousel.Caption className='slide-caption'>
                         <h3>Third slide label</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                     </Carousel.Caption>
